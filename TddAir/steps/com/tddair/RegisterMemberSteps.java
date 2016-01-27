a12f6376257192f8172a1fdeb2c45d5386315a29
Steve Fraser <swbfraser@gmail.com>
2016-01-26 23:19:19 -0600

    Second Scenario worked, but found error in step files. Need more unit tests to prevent multiple entries of "" -- but probably not something that the scenarios should be concerned about.

--- TddAir/steps/com/tddair/RegisterMemberSteps.java
+++ TddAir/steps/com/tddair/RegisterMemberSteps.java
@@ -1,8 +1,10 @@
 package com.tddair;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 
 public class RegisterMemberSteps {
 	
@@ -20,6 +22,10 @@ public class RegisterMemberSteps {
 
 	@Then("^the traveler is \"([^\"]*)\" as a member$")
 	public void the_traveler_is_as_a_member(String arg1) throws Throwable {
-	    membership.getMember(userName);
+	    
+		Member result = membership.getMember(userName);
+		assertNotNull(result);
+		assertTrue(userName.equalsIgnoreCase(result.getIdentifier()));
+		assertTrue(email.equalsIgnoreCase(result.getEmail()));
 	}
 }
