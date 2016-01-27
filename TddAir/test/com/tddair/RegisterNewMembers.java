7304315a756e59ab4676a93cde66c4b57c7d5ddc
Steve Fraser <swbfraser@gmail.com>
2016-01-26 21:49:20 -0600

    when No Membership then count is zero.

---
+++ TddAir/test/com/tddair/RegisterNewMembers.java
@@ -0,0 +1,19 @@
+package com.tddair;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RegisterNewMembers {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void whenNoMembershipCountIsZero() {
+		fail("Not yet implemented");
+	}
+
+}
