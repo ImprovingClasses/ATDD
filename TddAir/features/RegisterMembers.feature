f5deef7c4f59d7a15307c217a87f9d080b8b98ea
Steve Fraser <swbfraser@gmail.com>
2016-01-27 00:37:02 -0600

    Retain email even when user does not enter username.  Fixed -- previous feature files are not working for this.

--- TddAir/features/RegisterMembers.feature
+++ TddAir/features/RegisterMembers.feature
@@ -8,6 +8,7 @@ Scenario: Register an new member not having conflicts.
 When the traveler registeres with a username "bob" and the email "bob@abc.com"
 Then the traveler is "registered" as a member 
 
-Scenario: Members are can register with only an email
-When the traveler registers with a username "" and the email "bob@abc.com"
-Then the traveler is "registered" as a member
+
+Scenario: Record email when member does not enter username.
+When the traveler registeres with a username "" and the email "bob@abc.com"
+Then the traveler is "added" to email list 
--- TddAir/steps/com/tddair/RegisterMemberSteps.java
+++ TddAir/steps/com/tddair/RegisterMemberSteps.java
@@ -5,27 +5,32 @@ import cucumber.api.java.en.When;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-
 public class RegisterMemberSteps {
-	
+
 	Membership membership = new Membership();
 	String userName;
 	String email;
-	
+
 	@When("^the traveler registeres with a username \"([^\"]*)\" and the email \"([^\"]*)\"$")
 	public void the_traveler_registeres_with_a_username_and_the_email(String userName, String email) throws Throwable {
-	    this.userName = userName;
-	    this.email = email;
-	    
-	    membership.enroll(userName, email);
+		this.userName = userName;
+		this.email = email;
+
+		membership.enroll(userName, email);
 	}
 
 	@Then("^the traveler is \"([^\"]*)\" as a member$")
 	public void the_traveler_is_as_a_member(String arg1) throws Throwable {
-	    
+
 		Member result = membership.getMember(userName);
 		assertNotNull(result);
 		assertTrue(userName.equalsIgnoreCase(result.getIdentifier()));
 		assertTrue(email.equalsIgnoreCase(result.getEmail()));
 	}
+
+	@Then("^the traveler is \"([^\"]*)\" to email list$")
+	public void the_traveler_is_to_email_list(String added) throws Throwable {
+		assertTrue("added".equals(added) == membership.isEmailRecorded(email));
+	}
+
 }
