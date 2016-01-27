13ce936a47d5dffa03eea0c82e2ea1da886a7e9e
Steve Fraser <swbfraser@gmail.com>
2016-01-26 23:03:09 -0600

    Update Step definition file to pass the tests.

--- TddAir/steps/com/tddair/RegisterMemberSteps.java
+++ TddAir/steps/com/tddair/RegisterMemberSteps.java
@@ -6,15 +6,20 @@ import cucumber.api.java.en.When;
 
 public class RegisterMemberSteps {
 	
+	Membership membership = new Membership();
+	String userName;
+	String email;
+	
 	@When("^the traveler registeres with a username \"([^\"]*)\" and the email \"([^\"]*)\"$")
-	public void the_traveler_registeres_with_a_username_and_the_email(String arg1, String arg2) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void the_traveler_registeres_with_a_username_and_the_email(String userName, String email) throws Throwable {
+	    this.userName = userName;
+	    this.email = email;
+	    
+	    membership.enroll(userName, email);
 	}
 
 	@Then("^the traveler is \"([^\"]*)\" as a member$")
 	public void the_traveler_is_as_a_member(String arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    membership.getMember(userName);
 	}
 }
