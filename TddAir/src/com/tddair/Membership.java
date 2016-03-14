a8aeca4ba7ba6502b3efb1a1f0ebbaac2784122a
Test <Test@Computer12>
2016-03-14 15:12:23 -0500

    commit 1

---
+++ TddAir/src/com/tddair/Membership.java
@@ -0,0 +1,18 @@
+package com.tddair;
+
+public class Membership {
+	
+	private int count = 0;
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+	
+	
+
+}
--- TddAir/test/com/tddair/RegisterMemberTest.java
+++ TddAir/test/com/tddair/RegisterMemberTest.java
@@ -1,5 +1,6 @@
 package com.tddair;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class RegisterMemberTest {
@@ -7,7 +8,24 @@ public class RegisterMemberTest {
 	
 	@Test
 	public void whenMemberCountIsZero() {
+		Membership membership = new Membership();
+		Assert.assertEquals(0, membership.getCount());	
+	}
+	
+	@Test
+	public void whenMemberCountIsOne() {
+		Membership membership = new Membership();
+		membership.setCount(1);
+		Assert.assertEquals(1, membership.getCount());	
+	}
+	
+	
+	@Test
+	public void RetrieveByMemberIdentifierReturnsmatch() {
 		
 	}
+	
+	
+	
 
 }
