b6d6c003029bc82e31c4645c046ed8a94a5a18ce
Test <Test@Computer12>
2016-03-14 14:53:37 -0500

    initial

---
+++ TddAir/steps/RegisterMemberSteps.java
@@ -0,0 +1,38 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class RegisterMemberSteps {
+
+	
+	@When("^Entered username is \"([^\"]*)\"$")
+	public void entered_username_is(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Entered email is \"([^\"]*)\"$")
+	public void entered_email_is(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^\"([^\"]*)\" is \"([^\"]*)\" to our list$")
+	public void is_to_our_list(String arg1, String arg2) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^Existing username is \"([^\"]*)\"$")
+	public void existing_username_is(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^Existing email is \"([^\"]*)\"$")
+	public void existing_email_is(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+}
