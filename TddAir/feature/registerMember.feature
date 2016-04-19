e90b5234597cb97a5ad323e5ed860624aaa2268a
Test <Test@10.120.0.30>
2016-04-19 13:46:38 -0500

    newFileis added

---
+++ TddAir/feature/registerMember.feature
@@ -0,0 +1,28 @@
+Feature: register member
+As a Traveller I want to register
+
+Scenario: Register member
+When user "bob" and email "bob@abc.com"
+Then "bob" is added
+
+Scenario: Register member doesn't match name and email id
+When user "bob" and email "aro@abc.com"
+Then "bob" is added
+
+Scenario: Register member doesn't have name and valid email id
+When user "" and email "aro@abc.com"
+Then "bob" is added
+
+Scenario: Register member doesn't have email and valid name
+When user "Aro" and email ""
+Then "Aro" is not added
+
+
+Scenario: Register member doesn't have name and email
+When user "" and email ""
+Then user is not added
+
+Scenario: Register member that is already exist
+when user name "Aro" and email id "Aro@Aro.com"
+Then "Aro" is not added
+
---
+++ TddAir/steps/registraterMember.java
@@ -0,0 +1,30 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class registraterMember {
+	
+	@When("^user \"([^\"]*)\" and email \"([^\"]*)\"$")
+	public void user_and_email(String arg1, String arg2) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^\"([^\"]*)\" is added$")
+	public void is_added(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^\"([^\"]*)\" is not added$")
+	public void is_not_added(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^user is not added$")
+	public void user_is_not_added() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+}
