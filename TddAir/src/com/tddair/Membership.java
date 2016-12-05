c3ab21bb02f2a5728f2dd5eec74b091dbeb43b7b
Test <Test@172.31.98.72>
2016-12-05 16:33:31 -0600

    test case 4

--- TddAir/src/com/tddair/Membership.java
+++ TddAir/src/com/tddair/Membership.java
@@ -1,13 +1,17 @@
 package com.tddair;
 
+import java.util.ArrayList;
+
 public class Membership {
 
 	int size;
 	Member m1;
+	ArrayList<Member> memList;
 	
 	public Membership() {
 		size = 0;
 		m1 = null;
+		memList = new ArrayList<>();
 	}
 	
 	public int getNumMembers() { return size; }
@@ -16,10 +20,19 @@ public class Membership {
 	{
 		size++;
 		m1 = new Member(userID, email);
+		memList.add(m1);
 	}
 	
 	public Member getMember(String userID)
-	{
+	{	
+		for (Member member : memList)
+		{
+			if (member.getUserID() == userID)
+					return member;
+		}
+		
 		return m1;
 	}
+	
+	
 }
