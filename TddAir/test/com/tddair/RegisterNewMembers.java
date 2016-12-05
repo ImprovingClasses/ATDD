eb659200580c371a9be2557e42c5f83d96e97c3c
Heather Edington <Test@172.31.98.63>
2016-12-05 15:59:29 -0600

    RegisterNewMembers.java

---
+++ TddAir/test/com/tddair/RegisterNewMembers.java
@@ -0,0 +1,16 @@
+package com.tddair;
+
+import static org.junit.Assert.*;
+
+public class RegisterNewMembers {
+	@Before
+	public void setUp() throws Exception{
+		
+	}
+	
+	@Test 
+	public void whenNoMembershipCountIsZero() {
+		fail("No yet implemented");
+	}
+
+}
