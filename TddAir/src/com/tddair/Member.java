e9a8a1b04ed4a272d08b56dadc78f824f6b5448d
Test <Test@172.31.98.212>
2016-12-05 16:44:47 -0600

    Add Member Class

---
+++ TddAir/src/com/tddair/Member.java
@@ -0,0 +1,22 @@
+package com.tddair;
+
+public class Member {
+	
+	private String UserID;
+	private String email;
+	public String getUserID() {
+		return UserID;
+	}
+	public void setUserID(String userID) {
+		UserID = userID;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
--- TddAir/src/com/tddair/Membership.java
+++ TddAir/src/com/tddair/Membership.java
@@ -5,8 +5,21 @@ import java.util.HashMap;
 public class Membership {
 	//HashMap<String, String> members = new HashMap<String, String>();
 
+	int membercount = 0;
 	public int getEnrollmentCount() {
-		return  0;
+		return  membercount;
+	}
+	
+	/*public void Memberhip(Member m){
+	 Member tDDMember = new Member();
+	 tDDMember.getUserID();
+	 tDDMember.getEmail();
+	}*/
+	
+	public void add(Member m){
+		//m.getUserID();
+		//m.getEmail();
+		membercount++; 
 	}
 
 
