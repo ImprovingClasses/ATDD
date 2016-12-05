4175b4ee47fd987d66ea42aec095c316b41337f2
Test <Test@172.31.98.72>
2016-12-05 16:22:59 -0600

    test case 3

--- TddAir/src/com/tddair/Membership.java
+++ TddAir/src/com/tddair/Membership.java
@@ -3,9 +3,11 @@ package com.tddair;
 public class Membership {
 
 	int size;
+	Member m1;
 	
 	public Membership() {
 		size = 0;
+		m1 = null;
 	}
 	
 	public int getNumMembers() { return size; }
@@ -13,10 +15,14 @@ public class Membership {
 	public void addMember(String userID, String email)
 	{
 		size++;
+		m1 = new Member(userID, email);
 	}
 	
 	public Member getMember(String userID)
 	{
-		return null;
+		
+		
+		return m1;
+		
 	}
 }
