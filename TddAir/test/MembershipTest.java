5c91e6d8be476062546a854062bb0e24003d7719
Test <Test@172.31.98.68>
2016-12-05 15:53:34 -0600

    Create initial failing test

---
+++ TddAir/test/MembershipTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import com.tddair.*;
+
+public class MembershipTest {
+
+	@Test public void initialMembershipCountShouldBeZero()
+	{
+		Membership testObj = new Membership();
+		assertEquals(testObj.getNumMembers(),0);
+	}
+}
