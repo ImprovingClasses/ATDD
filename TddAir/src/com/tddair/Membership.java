c466661b2d54081dcc67f687a4acfae4abc6d7d5
Test <Test@Instructor>
2016-12-05 16:25:20 -0600

    Implementd method

--- TddAir/src/com/tddair/Membership.java
+++ TddAir/src/com/tddair/Membership.java
@@ -5,13 +5,20 @@ import java.util.HashMap;
 
 public class Membership {
 
-	private HashMap<String,ArrayList<String>> members = new HashMap<String,ArrayList<String>>();
+	private HashMap<String, ArrayList<String>> members = new HashMap<String, ArrayList<String>>();
 
-	public int enrollmentCount() {		
+	public int enrollmentCount() {
 		return members.size();
-	} 
+	}
 
-
-
+	public void addNewMember(String userID, String emailAddress) {
+		if (members.containsKey(emailAddress)) {
+			members.get(emailAddress).add(userID);
+		} else {
+            ArrayList<String> list = new ArrayList<String>();
+            list.add(userID);
+            members.put(emailAddress, list);
+		}
+	}
 
 }
