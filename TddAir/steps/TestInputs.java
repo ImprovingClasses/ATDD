20e92096fbbd15a935b92918bdd8c270c71ab21c
Heather Edington <Test@172.31.98.63>
2016-12-05 14:58:10 -0600

    Latest

---
+++ TddAir/steps/TestInputs.java
@@ -0,0 +1,23 @@
+	import cucumber.api.java.en.When;
+	import cucumber.api.java.en.Then;
+	import cucumber.api.PendingException;
+	
+public class TestInputs {
+	
+
+@When("^user registers with a username \"([^\"]*)\" and the email \"([^\"]*)\"$")
+public void user_registers_with_a_username_and_the_email(String arg1, String arg2) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Then("^user is \"([^\"]*)\" for reward program$")
+public void user_is_for_reward_program(String arg1) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+
+	
+
+}
