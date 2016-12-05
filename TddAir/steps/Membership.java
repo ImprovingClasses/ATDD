6234bc833a3e95a01c89c5bb7a6aea07f5af49c2
Test <Test@172.31.99.6>
2016-12-05 15:02:36 -0600

    commit

--- TddAir/steps/Membership.java
+++ TddAir/steps/Membership.java
@@ -14,7 +14,7 @@ public class Membership {
 	@Then("^the traveler is \"([^\"]*)\" as a member$")
 	public void the_traveler_is_as_a_member(String arg1) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-	   // throw new PendingException();
+	   //    throw new PendingException();
 	}
 
 
