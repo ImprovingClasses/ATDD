001338312f2c15b59c9ca0b80d79321e3a6ad238
Seeola <Seeola@172.31.99.169>
2016-12-05 16:22:49 -0600

    Checking in tests

---
+++ TddAir/src/com/tddair/Membership.java
@@ -0,0 +1,13 @@
+package com.tddair;
+
+import java.util.HashMap;
+
+public class Membership {
+	//HashMap<String, String> members = new HashMap<String, String>();
+
+	public int getEnrollmentCount() {
+		return  0;
+	}
+
+
+}
---
+++ TddAir/test/com/tddair/MembershipTest.java
@@ -0,0 +1,29 @@
+package com.tddair;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import org.junit.Before;
+
+
+public class MembershipTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void countMemberZero() {
+		Membership member = new Membership();
+		assertEquals(0, member.getEnrollmentCount());
+	}
+	
+	@Test
+	public void oneMemberAddedCountShouldBeOne(){
+		Membership member = new Membership();
+		
+		member.add(new Member());
+		assertEquals(1, member.getEnrollmentCount());
+	}
+
+}
