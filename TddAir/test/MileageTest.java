1097dbf32e39000ecf208fde582f99016f5c5bf8
Test <Test@172.31.98.72>
2016-12-06 13:41:51 -0600

    init mileage is zero

--- TddAir/test/MileageTest.java
+++ TddAir/test/MileageTest.java
@@ -1,5 +1,8 @@
 import com.tddair.*;
 
 public class MileageTest {
-
+@Test public void initialMileageIsZero() {
+	Member memObj = new Member("userID", "email");
+	assertEquals(memObj.getMileage(),0);
+}
 }
