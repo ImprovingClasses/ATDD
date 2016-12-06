c27310b6c28e164a8d2bdbc4115051b6919181c4
Test <Test@172.31.98.72>
2016-12-06 13:47:40 -0600

    add mileage

--- TddAir/test/MileageTest.java
+++ TddAir/test/MileageTest.java
@@ -10,4 +10,11 @@ public class MileageTest {
 		Member memObj = new Member("userID", "email");
 		assertEquals(memObj.getMileage(),0);
 	}
+	
+	@Test public void addMileage() {
+		Member memObj = new Member("userID", "email");
+		int x = 14999;
+		memObj.addMiles(x);
+		assertEquals(memObj.getMileage(),x);
+	}
 }
