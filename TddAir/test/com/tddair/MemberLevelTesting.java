482030dcade2a765dc4640e2582e7c69f5d76476
Heather Edington <Test@172.31.98.63>
2016-12-06 14:39:34 -0600

    Added more test cases

--- TddAir/test/com/tddair/MemberLevelTesting.java
+++ TddAir/test/com/tddair/MemberLevelTesting.java
@@ -36,5 +36,48 @@ public class MemberLevelTesting {
 		String level = subject.getLevel("bob");
 		assertEquals( "Blue", level);
 	}
+	@Test 
+	public void whenNewMemberFliesFiveHundredMiles() {
+		Membership subject = new Membership();
+		subject.enroll("bob", "bob@abc.com");
+		subject.addMileage("bob", 500);
+		String level = subject.getLevel("bob");
+		assertEquals( "Red", level);
+	}
+	@Test 
+	public void whenMemberNewMemberGoesGolden() {
+		Membership subject = new Membership();
+		subject.enroll("bob", "bob@abc.com");
+		subject.addMileage("bob", 75000);
+		String level = subject.getLevel("bob");
+		assertEquals( "Golden", level);
+	}
+	@Test 
+	public void whenMemberNewMemberFlies49999() {
+		Membership subject = new Membership();
+		subject.enroll("bob", "bob@abc.com");
+		subject.addMileage("bob", 49999);
+		String level = subject.getLevel("bob");
+		assertEquals( "Green", level);
+	}
+	@Test 
+	public void whenMemberNewMemberFlies74999() {
+		Membership subject = new Membership();
+		subject.enroll("bob", "bob@abc.com");
+		subject.addMileage("bob", 74999);
+		String level = subject.getLevel("bob");
+		assertEquals( "Blue", level);
+	}
+	@Test 
+	public void whenExistingMemberGoesFromRedToGreen() {
+		Membership subject = new Membership();
+		subject.enroll("bob", "bob@abc.com");
+		subject.addMileage("bob", 24999);
+		String level = subject.getLevel("bob");
+		assertEquals( "Red", level);
+		subject.addMileage("bob", 13000);
+		level = subject.getLevel("bob");
+		assertEquals( "Green", level);
+	}
 
 }
