4dc528cc454a2721b5804bc8664dc9add55c2bdc
Test <Test@172.31.98.67>
2016-12-06 16:46:35 -0600

    Book Flight Tests

--- TddAir/test/FlightMilesTest.java
+++ TddAir/test/FlightMilesTest.java
@@ -1,6 +1,23 @@
+import org.junit.Test;
+
+import com.tddair.Flight;
+import com.tddair.Member;
 
 public class FlightMilesTest {
 
+	@Test public void bookGoodFlight()
+	{
+		Member memObj = new Member("user", "email");
+		Flight flight = new Flight("AAA","BBB", 123, "TD", 123);
+		memObj.bookFlight(flight);
+		assertEquals(memObj.getMileage(), 123);
+	}
 	
-	
+	@Test public void bookBadFlight()
+	{
+		Member memObj = new Member("user", "email");
+		Flight flight = new Flight("AAA","BBB", 123, "AA", 123);
+		memObj.bookFlight(flight);
+		assertEquals(memObj.getMileage(), 0);
+	}
 }
