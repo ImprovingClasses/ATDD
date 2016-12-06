e1277f883e56562078f64c1141ad8cb0272149d9
Test <Test@172.31.99.133>
2016-12-06 14:09:01 -0600

    test 1

--- TddAir/test/com/tddair/UpdateMemberStatus.java
+++ TddAir/test/com/tddair/UpdateMemberStatus.java
@@ -2,6 +2,8 @@ package com.tddair;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 
@@ -9,16 +11,34 @@ import org.junit.Test;
 
 public class UpdateMemberStatus {
 	
-	private static final String STATUS_RED = "red";
-	private static final String STATUS_GREEN = "green";
-	private static final String STATUS_BLUE = "blue";
-	private static final String STATUS_GOLD = "gold";
+	private static final String STATUS_RED = "Red";
+	private static final String STATUS_GREEN = "Green";
+	private static final String STATUS_BLUE = "Blue";
+	private static final String STATUS_GOLD = "Gold";
 
 	@Test
 	public void whenUserHasZeroMilesAnddAddsZeroMilesStatusIsRed() {
 		Membership subject = new Membership();
 		subject.addNewMember("bob","bob@abc.com",0);
+		addMiles(subject, "bob@abc.com", 0);
+		ArrayList<Member> members = subject.getMember("bob@abc.com");
+		for (Member member: members) {
+			if (member.getUserId().equals("bob")) {
+				assertEquals(STATUS_RED, member.getStatus());
+				assertEquals(0, member.getMileage());
+			}
+		}
 		
+		
+	}
+	
+	private void addMiles(Membership subject, String email, int miles) {
+		ArrayList<Member> members = subject.getMember("bob@abc.com");
+		for (Member member: members) {
+			if (member.getUserId().equals("bob")) {
+				member.addMiles(0);
+			}
+		}
 	}
 
 }
