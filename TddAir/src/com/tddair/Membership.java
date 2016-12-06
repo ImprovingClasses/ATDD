5b3ca5f46a89ee9efecfd670695c520e3e180cba
Test <Test@172.31.99.1>
2016-12-06 09:56:53 -0600

    Added code for adding duplicate user

--- TddAir/src/com/tddair/Membership.java
+++ TddAir/src/com/tddair/Membership.java
@@ -11,19 +11,36 @@ public class Membership {
 		return members.size();
 	}
 
-	public void addNewMember(String userID, String emailAddress) {
+	public boolean addNewMember(String userID, String emailAddress) {
+		boolean result = true;
+
 		if (members.containsKey(emailAddress)) {
-			members.get(emailAddress).add(new Member(userID, emailAddress));
+			if (containsID(members.get(emailAddress), userID)) {
+				result = false;
+			} else {
+				members.get(emailAddress).add(new Member(userID, emailAddress));
+			}
 		} else {
-            ArrayList<Member> list = new ArrayList<Member>();
-            list.add(new Member(userID, emailAddress));
-            
-            members.put(emailAddress, list);
+			ArrayList<Member> list = new ArrayList<Member>();
+			list.add(new Member(userID, emailAddress));
+			members.put(emailAddress, list);
 		}
+		return result;
 	}
 
 	public ArrayList<Member> getMember(String emailAddress) {
 		return members.get(emailAddress);
+	}
+
+	private boolean containsID(ArrayList<Member> memberList, String memberID) {
+		boolean result = false;
+		for (Member member : memberList) {
+			if (member.getUserId().equals(memberID)) {
+				result = true;
+				break;
+			}
+		}
+		return result;
 	}
 
 }
