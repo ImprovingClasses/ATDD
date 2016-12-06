f215b21a6d07edc22ade5914c9dd46a541d9e9cc
Test <Test@172.31.99.1>
2016-12-06 16:20:16 -0600

    add

--- TddAir/test/com/tddair/UpdateMemberStatus.java
+++ TddAir/test/com/tddair/UpdateMemberStatus.java
@@ -1,12 +1,11 @@
 package com.tddair;
 
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
