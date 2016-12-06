c5bc6198783814779877b0eae7e55ab986cd30da
Test1 <Test@172.31.99.48>
2016-12-06 09:46:55 -0600

    Adding double test

--- TddAir/test/com/tddair/TestMemebership.java
+++ TddAir/test/com/tddair/TestMemebership.java
@@ -29,6 +29,28 @@ public class TestMemebership {
 		Member m = members.getById("bob");
 		assertEquals("Test successful member match",  "bob", m.getId());
 		assertEquals("Test successful email match", "bob@abc.com", m.getEmail());
+		Member n = members.getByEmail("bob@abc.com");
+		assertEquals("Test successful member match",  "bob", n.getId());
+		assertEquals("Test successful email match", "bob@abc.com", n.getEmail());
+	}
+	
+	@Test
+	public void whenTwoMembersAddedThenRetrieveMemberAttributesShouldMatch(){
+		Membership members = new Membership();
+		members.addNewMember("bob", "bob@abc.com");
+		members.addNewMember("Sue", "sue@abc.com");
+		Member bob1 = members.getById("bob");
+		Member bob2 = members.getByEmail("bob@abc.com");
+		assertEquals("Test successful member match", "bob", bob1.getId());
+		assertEquals("Test successful email match", "bob@abc.com", bob1.getEmail());
+		assertEquals("Test successful member match", "bob", bob2.getId());
+		assertEquals("Test successful email match", "bob@abc.com", bob2.getEmail());
+		Member sue1 = members.getByEmail("sue@abc.com");
+		Member sue2 = members.getByEmail("sue@abc.com");
+		assertEquals("Test successful member match", "Sue", sue1.getId());
+		assertEquals("Test successful email match", "sue@abc.com", sue1.getEmail());
+		assertEquals("Test successful member match", "Sue", sue2.getId());
+		assertEquals("Test successful email match", "sue@abc.com", sue2.getEmail());
 	}
 	
 }
