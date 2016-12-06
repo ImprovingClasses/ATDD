7149fd1e45a5199ff87f9d8ccb53195dff90e665
Test <Test@172.31.99.133>
2016-12-06 11:20:08 -0600

    update for working cucumber

--- TddAir/steps/RegisterUserSteps.java
+++ TddAir/steps/RegisterUserSteps.java
@@ -1,18 +1,23 @@
-import cucumber.api.PendingException;
+import com.tddair.TddAirApplication;
+
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+import static org.junit.Assert.*;
+
 public class RegisterUserSteps {
 
-	@When("^the user registers with the username \"([^\"]*)\" and email \"([^\"]*)\"$")
-	public void the_user_registers_with_the_username_and_email(String arg1, String arg2) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	TddAirApplication controller = new TddAirApplication();
+	boolean isMemberAdded = true;
+	
+	
+	@When("^the traveler registers with a userid \"([^\"]*)\" and the email \"([^\"]*)\"$")
+	public void the_traveler_registers_with_a_userid_and_the_email(String arg1, String arg2) throws Throwable {
+		isMemberAdded = controller.addMember(arg1, arg2);
 	}
 
-	@Then("^the traveller is \"([^\"]*)\" as a member$")
-	public void the_traveller_is_as_a_member(String arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	@Then("^the traveler is \"([^\"]*)\" as a member$")
+	public void the_traveler_is_as_a_member(String arg1) throws Throwable {
+		assertEquals(isMemberAdded,true);
 	}
 }
