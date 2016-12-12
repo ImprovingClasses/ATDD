e16fccfce41da91e2ef9ac1373071a20fe931c24
Steve Fraser <steve.fraser@improving.com>
2016-12-12 15:50:26 -0600

    Step File Added
    All cucumber tests are in pending state.

---
+++ TddAir/step/RegisterMembershipStep.java
@@ -0,0 +1,32 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class RegisterMembershipStep {
+
+    @When("^Traveler selects identifier \"([^\"]*)\" and email \"([^\"]*)\" when registering$")
+    public void traveler_selects_identifier_and_email_when_registering(String arg1, String arg2) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+    
+    @Then("^The traveler information is \"([^\"]*)\"$")
+    public void the_traveler_information_is(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+    
+    @When("^Traveler retrieves identifier \"([^\"]*)\" information$")
+    public void traveler_retrieves_identifier_information(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+    
+    @Then("^The traveler information retrieved is \"([^\"]*)\" and \"([^\"]*)\"$")
+    public void the_traveler_information_retrieved_is_and(String arg1, String arg2) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+}
