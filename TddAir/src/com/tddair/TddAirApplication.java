05acb3cd2ee65b73a7133027bb9e304bee5a9fa2
Steve Fraser <steve.fraser@improving.com>
2016-12-14 13:05:45 -0600

    Step files implemented. One error reveiled.

--- TddAir/src/com/tddair/TddAirApplication.java
+++ TddAir/src/com/tddair/TddAirApplication.java
@@ -4,7 +4,7 @@ package com.tddair;
 public class TddAirApplication {
 	
 	private FlightProvider flights;
-	
+	private MembershipManagmentCollection membershipCollection = new MembershipManagmentCollection();
 	
 	//The default Constructor is provided for the Classroom only 
 	//to give easy starter file for TddAirApplication
@@ -21,5 +21,13 @@ public class TddAirApplication {
 	
 	public void addFlight(String origin, String destination, int mileage, String airline, int number) {
 		flights.addFlight(origin, destination, mileage, airline, number);
+	}
+	
+	public boolean enroll(String identifier, String email) {
+	    return membershipCollection.enroll(identifier, email);
+	}
+	
+	public Member getMember(String identifier){
+	    return membershipCollection.getMember(identifier);
 	}
 }
--- TddAir/step/com/tddair/RegisterMembershipStep.java
+++ TddAir/step/com/tddair/RegisterMembershipStep.java
@@ -1,32 +1,36 @@
 package com.tddair;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class RegisterMembershipStep {
 
+    TddAirApplication app = new TddAirApplication();
+    boolean recorded = false;
+    Member member;
+    
     @When("^Traveler selects identifier \"([^\"]*)\" and email \"([^\"]*)\" when registering$")
-    public void traveler_selects_identifier_and_email_when_registering(String arg1, String arg2) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void traveler_selects_identifier_and_email_when_registering(String identifier, String email) throws Throwable {
+        recorded = app.enroll(identifier, email);
     }
     
     @Then("^The traveler information is \"([^\"]*)\"$")
-    public void the_traveler_information_is(String arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void the_traveler_information_is(String result) throws Throwable {
+        assertTrue("recorded".equalsIgnoreCase(result)==recorded);
     }
     
     @When("^Traveler retrieves identifier \"([^\"]*)\" information$")
-    public void traveler_retrieves_identifier_information(String arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void traveler_retrieves_identifier_information(String identifier) throws Throwable {
+       member = app.getMember(identifier);
     }
     
     @Then("^The traveler information retrieved is \"([^\"]*)\" and \"([^\"]*)\"$")
-    public void the_traveler_information_retrieved_is_and(String arg1, String arg2) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void the_traveler_information_retrieved_is_and(String identifier, String email) throws Throwable {
+        assertEquals(identifier, member.getIdentifier());
+        assertEquals(email, member.getEmail());
     }
 
 }
